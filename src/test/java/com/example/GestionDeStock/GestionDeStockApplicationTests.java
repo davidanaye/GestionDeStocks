05dd3ca50618e42cbfb64738ca873ced1e483fd3
davidanaye <kingeliezer13@gmail.com>
2023-12-07 17:35:57 +0100

    Mon premier commit

---
+++ src/test/java/com/example/GestionDeStock/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
